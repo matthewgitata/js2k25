4c06c8be0b9bf8640a230f217a27ae8599d01241
Matthew Gitata <matthewgitata@gmail.com>
2025-01-14 22:33:33 +0300

    Tip Calculator.

---
+++ tip-calculator/script.js
@@ -0,0 +1,9 @@
+var bill = Number(prompt("Enter bill amount: "));
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value was ${
+    bill + tip
+  }`
+);
