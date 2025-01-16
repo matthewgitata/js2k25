9e89e4966bcfc1a47a8e249d7991f13905b4774f
Matthew Gitata <matthewgitata@gmail.com>
2025-01-16 14:41:14 +0300

    Tip calculator v2.

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,10 +1,28 @@
-const bills = [125, 555, 44];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const tips = [];
 
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+const totals = [];
 
-console.log(totals[0], totals[1], totals[2]);
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+}
 
 function calcTip(bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
