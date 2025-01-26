d2a3d1094d5312a5a7d46499db5469b3de5f25b9
Matthew Gitata <matthewgitata@gmail.com>
2025-01-26 05:00:00 +0300

    Dogs project.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const greet = (greeting) => {
-  return (name) => {
-    console.log(`${greeting} ${name}`);
-  };
+dogsJulia = [3, 5, 2, 12, 17];
+dogsKate = [4, 1, 5, 18, 3];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const realDogsJulia = dogsJulia.slice(1, -2);
+
+  const allDogs = [...realDogsJulia, ...dogsKate];
+
+  allDogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy.`);
+    }
+  });
 };
 
-greet("Hello")("Maina");
+checkDogs(dogsJulia, dogsKate);
