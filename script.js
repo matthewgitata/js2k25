df779cea5d2a91873106ef336f24bf8e06dac2ff
Matthew Gitata <matthewgitata@gmail.com>
2025-01-27 07:11:11 +0300

    Dogs project v2.

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ const checkDogs = function (dogsJulia, dogsKate) {
       console.log(`Dog number ${i + 1} is still a puppy.`);
     }
   });
+  const calcAverageHumanAge = function (ages) {
+    const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+    const adults = humanAges.filter((age) => age >= 18);
+    const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+    return average;
+  };
+  console.log(calcAverageHumanAge(allDogs));
 };
 
 checkDogs(dogsJulia, dogsKate);
