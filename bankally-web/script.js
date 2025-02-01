06cf33f8e3a31c0e40ba2c55966ef9553fbb9455
Matthew Gitata <matthewgitata@gmail.com>
2025-02-01 18:18:18 +0300

    BAWeb - lazy load images.

--- bankally-web/script.js
+++ bankally-web/script.js
@@ -12,6 +12,7 @@ const tabsContainer = document.querySelector(".operations__tab-container");
 const tabtsContent = document.querySelectorAll(".operations__content");
 const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
+const imgTargets = document.querySelectorAll("img[data-src]");
 
 ///////////////////////////////////////
 // Modal window
@@ -126,3 +127,23 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+// Lazy loading images
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imgTargets.forEach((img) => imgObserver.observe(img));
