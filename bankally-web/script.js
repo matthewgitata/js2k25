42109ad6f082db1b2305a5089d40d50808ef79cc
Matthew Gitata <matthewgitata@gmail.com>
2025-02-01 13:24:24 +0300

    BAWeb - sticky navigation on scroll.

--- bankally-web/script.js
+++ bankally-web/script.js
@@ -89,3 +89,13 @@ const handleHover = function (e) {
 // Passing "argument" into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener("scroll", function () {
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
