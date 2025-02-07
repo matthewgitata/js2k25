1401527c72831e3df547607acccd941f85d0c1bf
Matthew Gitata <matthewgitata@gmail.com>
2025-02-07 06:06:33 +0300

    Lottery draw.

--- script.js
+++ script.js
@@ -1,56 +1,28 @@
-"use strict";
-
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
-
-// NEW COUNTRIES API URL (use instead of the URL shown in videos):
-// https://restcountries.com/v2/name/portugal
-
-// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
-// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
-
-///////////////////////////////////////
-
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText("beforeend", msg);
-};
-
-const renderCountry = function (data, className = "") {
-  const html = `
-        <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} m people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.languages[0].name}</p>
-          </div>
-        </article>
-  `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
-
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then((res) => {
-      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
-      return res.json();
-    })
-    .then((data) => {
-      return fetch(`http://restcountries.eu/rest/v2/name/${data.country}`);
-    })
-    .then((res) => {
-      if (!res.ok) throw new Error(`Country not found (${response.status})`);
-      return response.json();
-    })
-    .then((data) => renderCountry(data[0]))
-    .catch((err) => console.error(`${err.message}`));
-};
-
-btn.addEventListener("click", function () {
-  whereAmI(52.508, 13.381);
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("Lottery draw is happening");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("You WIN $");
+    } else {
+      reject(new Error("You lost your money ;-)"));
+    }
+  }, 2000);
 });
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (s) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, s * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log("I waited for 2 sec.");
+    return wait(1);
+  })
+  .then(() => console.log("I waited for 1 second."));
