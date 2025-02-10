92f50a844b550ec8e95eb38aa7f597902b182f08
Matthew Gitata <matthewgitata@gmail.com>
2025-02-10 14:33:33 +0300

    5 bread added to cart.

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log("Lottery draw is happening");
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve("You WIN $");
-    } else {
-      reject(new Error("You lost your money ;-)"));
-    }
-  }, 2000);
-});
+// Importing module
+import "./shopping-cart/shoppingCart.js";
+import { addToCart } from "./shopping-cart/shoppingCart.js";
 
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+console.log("Importing module");
 
-// Promisifying setTimeout
-const wait = function (s) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, s * 1000);
-  });
-};
-
-wait(2)
-  .then(() => {
-    console.log("I waited for 2 sec.");
-    return wait(1);
-  })
-  .then(() => console.log("I waited for 1 second."));
+addToCart("bread", 5);
---
+++ shopping-cart/shoppingCart.js
@@ -0,0 +1,16 @@
+// Exporting module
+
+console.log("Exproting module");
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
