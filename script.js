0b8e2b2cf618319b5d2445af689a870d4dd39cf5
Matthew Gitata <matthewgitata@gmail.com>
2025-02-11 09:36:33 +0300

    Fix bad code I.

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-// Importing module
-import "./shopping-cart/shoppingCart.js";
-import { addToCart } from "./shopping-cart/shoppingCart.js";
+const budget = [
+  { value: 250, description: "Sold old TV 📺", user: "jonas" },
+  { value: -45, description: "Groceries 🥑", user: "jonas" },
+  { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
+  { value: 300, description: "Freelancing 👩‍💻", user: "jonas" },
+  { value: -1100, description: "New iPhone 📱", user: "jonas" },
+  { value: -20, description: "Candy 🍭", user: "matilda" },
+  { value: -125, description: "Toys 🚂", user: "matilda" },
+  { value: -1800, description: "New Laptop 💻", user: "jonas" },
+];
 
-console.log("Importing module");
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
 
-addToCart("bread", 5);
+const addExpense = function (value, description, user) {
+  if (!user) user = "jonas";
+  user = user.toLowerCase();
+
+  const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+
+  if (value <= limit) {
+    budget.push({ value: -value, description: description, user: user });
+  }
+};
+addExpense(10, "Pizza 🍕");
+addExpense(100, "Going to movies 🍿", "Matilda");
+addExpense(200, "Stuff", "Jay");
+
+const checkExpenses = function () {
+  for (const entry of budget) {
+    const limit = spendingLimits[entry.user] ? spendingLimits[entry.user] : 0;
+
+    if (entry.value < -limit) {
+      entry.flag = "limit";
+    }
+  }
+};
+checkExpenses();
+
+const logBigExpenses = function (limit) {
+  let output = "";
+  for (const entry of budget) {
+    output += entry.value <= -limit ? `${entry.description.slice(-2)}  / ` : "";
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
+logBigExpenses(1000);
