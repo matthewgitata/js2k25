1b690c72277d4d5dfe98df5592188268fbf15f00
Matthew Gitata <matthewgitata@gmail.com>
2025-02-14 18:18:33 +0300

    Forkify - helper functions and configuration.

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -1,16 +1,6 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 
-const recipeContainer = document.querySelector(".recipe");
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
 
