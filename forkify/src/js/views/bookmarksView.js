3de1ea9c11adcc3d11aa2d6362593217aed18829
Matthew Gitata <matthewgitata@gmail.com>
2025-02-19 04:47:04 +0300

    Forkify - storing bookmarks with local storage.

--- forkify/src/js/views/bookmarksView.js
+++ forkify/src/js/views/bookmarksView.js
@@ -1,4 +1,3 @@
-import icons from "url:../../img/icons.svg";
 import previewView from "./previewView.js";
 import View from "./View";
 
@@ -6,6 +5,10 @@ class BookmarksView extends View {
   _parentElement = document.querySelector(".bookmarks__list");
   _errorMessage = "No bookmarks yet. Find a nice recipe and bookmark it ;)";
   _message = "";
+
+  addHandlerRender(handler) {
+    window.addEventListener("load", handler);
+  }
 
   _generateMarkup() {
     return this._data
